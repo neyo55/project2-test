3b1cb7049123a529b118849d86e8161981eaac4a
adeniyi rufai <kbneyo55@gmail.com>
2025-01-18 15:21:10 +0100

    edited main.py for logging

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import sys
 import os
+import logging
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename="flask.log",
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+    )
     app.run(host="0.0.0.0", port=5000, debug=True)
