513a6ce79e31a3e399b40236394e2d002327d185
adeniyi rufai <kbneyo55@gmail.com>
2025-01-18 14:42:36 +0100

    edited main.py

--- app/main.py
+++ app/main.py
@@ -4,5 +4,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from app import app
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
